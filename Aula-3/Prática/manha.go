0d06c3f3d5dcabeab816843972766fc9643f7ac4
Vinicius Oliveira <runnice@gmail.com>
2022-05-24 14:03:53 -0300

    ex-02

--- Aula-3/Prática/manha.go
+++ Aula-3/Prática/manha.go
@@ -1,64 +1,101 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
+	"text/tabwriter"
 )
 
-type Product struct {
+type produto struct {
 	Id         int
 	Quantidade int
 	Preco      float64
 }
 
-func registraProdutoComprado(p Product) {
-	arquivo, err := os.OpenFile("produtosComprados.csv", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+func (p produto) lineGeneratorCSV() string {
+	return fmt.Sprintf("%d, %.2f, %d\n", p.Id, p.Preco, p.Quantidade)
+}
+
+func (p produto) lineHeaderCSV() string {
+	return "ID, Preço, Quantidade\n"
+}
+
+func registraArquivo(nomeDoArquivo string, produtos []produto) error {
+	arquivo, err := os.OpenFile(nomeDoArquivo, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
+	}
+	if err != nil {
+		return fmt.Errorf("erro ao abrir o arquivo : %w", err)
 	}
 	defer arquivo.Close()
-	str := fmt.Sprintf("%d;%d;%.2f;\n", p.Id, p.Quantidade, p.Preco)
-	arquivo.WriteString(str)
-}
+	p := produtos[0]
 
-func imprimeArquivo(nomeDoArquivo string) {
-	arquivo, err := ioutil.ReadFile(nomeDoArquivo)
-	if err != nil {
-		fmt.Println("Ocorreu um erro:", err)
+	if _, err = arquivo.WriteString(p.lineHeaderCSV()); err != nil {
+		return fmt.Errorf("erro ao gerar primeira linhe: %w", err)
 	}
 
-	fmt.Println(string(arquivo))
+	for _, produto := range produtos {
+		if _, err = arquivo.WriteString(produto.lineGeneratorCSV()); err != nil {
+			fmt.Println("error to save line: ", err)
+		}
+	}
+	return nil
+
 }
 
-// func imprimeArquivo2(nomeDoArquivo string) ([]Product, error) {
-// 	f, err := os.Open(nomeDoArquivo)
-// 	if err != nil {
-// 		return []Product{}, err
-// 	}
-// 	defer f.Close()
-// 	rows := csv.NewReader(f)
-// 	var ps []Product
-// 	for {
-// 		row, err := rows.Read()
-// 		if err == io.EOF {
-// 			break
-// 		}
-// 		if err != nil {
-// 			return ps, err
-// 		}
+func lerArquivo(nomeDoArquivo string) {
 
-// 	}
-// 	return ps, nil
-// }
+	arquivo, err := os.Open(nomeDoArquivo)
+	if err != nil {
+		fmt.Println("Ocorreu um erro ao abrir o arquivo:", err)
+	}
+	defer arquivo.Close()
+
+	primeiraLinha := tabwriter.NewWriter(os.Stdout, 20, 30, 1, '\t', tabwriter.AlignRight)
+
+	scanner := bufio.NewScanner(arquivo)
+
+	scanner.Scan()
+
+	header := strings.Split(scanner.Text(), ";")
+
+	for _, c := range header {
+		fmt.Fprintf(primeiraLinha, "%s\t\t", c)
+	}
+	fmt.Fprintln(primeiraLinha)
+
+	var total float64
+	for scanner.Scan() {
+		valores := strings.Split(scanner.Text(), ",")
+		for i, v := range valores {
+			fmt.Fprintf(primeiraLinha, "%s\t\t", v)
+			if i == 0 {
+				preco, _ := strconv.ParseFloat(strings.TrimSpace(valores[1]), 64)
+				quantidade, _ := strconv.Atoi(strings.TrimSpace(valores[2]))
+				total += preco * float64(quantidade)
+			}
+
+		}
+
+		fmt.Fprintln(primeiraLinha)
+	}
+	fmt.Fprintf(primeiraLinha, "\t\t%.2f\n", total)
+
+	primeiraLinha.Flush()
+}
 
 func main() {
 
-	camisa := Product{1, 10, 29.90}
-	bola := Product{2, 4, 19.90}
+	var produtos = []produto{}
+	produtos = append(produtos, produto{Id: 1, Quantidade: 10, Preco: 100})
+	produtos = append(produtos, produto{Id: 2, Quantidade: 20, Preco: 200})
+	produtos = append(produtos, produto{Id: 3, Quantidade: 30, Preco: 300})
 
-	registraProdutoComprado(camisa)
-	registraProdutoComprado(bola)
+	registraArquivo("produtos.csv", produtos)
+	lerArquivo("produtos.csv")
 
-	imprimeArquivo("produtosComprados.csv")
 }
